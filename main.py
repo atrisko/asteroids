92032e1c99192ea2bc05ae3b37889a78f3048c8b
atrisko <alex.trisko@googlemail.com>
2025-11-09 14:12:27 +0100

    Created Game Screen

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from asteroids!")
+import pygame
+import constants
+from logger import log_state
 
+
+
+def game_loop():
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+     
+
+
+
+
+def main():
+    print("Starting Asteroids! with pygame version: ",pygame.version.ver)
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
+    game_loop()
+
