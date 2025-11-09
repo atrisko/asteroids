a27e8966d3b750255dc46229be3f0be94dd52c37
atrisko <alex.trisko@googlemail.com>
2025-11-09 18:50:44 +0100

    Added player object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+import player
 from logger import log_state
 
 
@@ -9,6 +10,12 @@ def game_loop():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updatable, drawable)
+    my_player = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    
+    
 
     while True:
         log_state()
@@ -16,6 +23,9 @@ def game_loop():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
@@ -28,6 +38,9 @@ def main():
     print("Starting Asteroids! with pygame version: ",pygame.version.ver)
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    
+    
+
 
 if __name__ == "__main__":
     main()
